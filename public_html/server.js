c4bfe43fc75ebffa31842e1d728121b81e90dd98
Jasper Ketelaar <jketelaar@student.tudelft.nl>
2017-01-09 15:39:58 +0100

    Working startup, need to look into output reading ('fs')?

--- public_html/server.js
+++ public_html/server.js
@@ -1,14 +1,14 @@
 var exec = require('child_process').exec;
-var mc = exec('(cd /root/spigot && exec java -jar spigot-1.11.2.jar > log.txt)',
-    function (error, stdout, stderr) {
-        console.log('stdout: ' + stdout);
-        console.log('stderr: ' + stderr);
-        if (error !== null) {
-            console.log('exec error: ' + error);
-        }
-    }
-);
+var spigot = function(callback) {
+    var init = exec('(cd /root/spigot && exec java -jar spigot-1.11.2.jar > log.txt)');
+    var list = [];
 
+    init.stdout.setEncoding('utf8');
+
+    spigot.stdout.on('data', function (data) {
+        console.log(data + " - Jasper");
+    });
+}
 var http = require('http');
 
 

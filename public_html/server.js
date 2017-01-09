f2246ff44372276685b1ec473141eb8b4f598dce
Jasper Ketelaar <jketelaar@student.tudelft.nl>
2017-01-09 21:59:31 +0100

    Made index

--- public_html/server.js
+++ public_html/server.js
@@ -1,24 +1,20 @@
+var fs = require('fs');
 var exec = require('child_process').exec;
-var spigot = function(callback) {
-    var init = exec('(cd /root/spigot && exec java -jar spigot-1.11.2.jar > log.txt)');
-    var list = [];
 
-    init.stdout.setEncoding('utf8');
+var spigot = exec('(cd /root/spigot && exec java -jar spigot-1.11.2.jar > log.txt)');
 
-    spigot.stdout.on('data', function (data) {
-        console.log(data + " - Jasper");
-    });
-}
+var file = "../spigot/log.txt";
+fs.watch(file, function (change) {
+    fs.readFile(file, function (err, data) {
+        if (!err)
+            console.log(data.toString('ascii'));
+    })
+});
+
 var http = require('http');
+var express = require('express');
+var app = express();
 
+app.use(express.static(__dirname + '/public'));
+http.createServer(app).listen(3000);
 
-/* minecraft.stdout.on('data', function (data) {
- console.log('stdout: ' + data);
- });
-
- minecraft.stderr.on('data', function (data) {
- console.log('stderr: ' + data);
- });
- minecraft.on('close', function (code) {
- console.log('closing code: ' + code);
- });*/

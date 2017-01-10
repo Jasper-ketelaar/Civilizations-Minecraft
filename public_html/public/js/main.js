b36c19cff3b31bf76dc4a094c49213cd54e10535
Jasper Ketelaar <jketelaar@student.tudelft.nl>
2017-01-11 00:37:38 +0100

    console log finished

--- public_html/public/js/main.js
+++ public_html/public/js/main.js
@@ -1,1 +1,43 @@
+const PAGES = ['dashboard', 'console', 'players', 'file manager', 'backup manager', 'server settings'];
 
+$(function () {
+    Console();
+
+    $.ajaxSetup({ cache: false });
+
+    $('li').on('click', 'a', function () {
+        reload($(this).parent().index());
+    });
+
+    var reload = function (state) {
+        $.get('/dashboard/' + PAGES[state] + ".html", function (data) {
+            $("#dashboard-section").find(".panel-body").html(data);
+        });
+    };
+
+    function Console() {
+
+        this.logs = 0;
+
+        this.load = function () {
+            var ul = document.getElementById('console-log');
+            if (ul !== undefined && ul !== null) {
+                $.get("/log", function (data) {
+                    var lines = data.split('\n');
+                    $.each(lines, function (index, string) {
+                        if (string !== "" && index + 2 > this.logs) {
+                            this.logs += 1;
+                            $(ul).append($("<li>").html(string));
+                            console.log(string);
+                        }
+                    }.bind(this));
+
+                }.bind(this));
+            }
+        }.bind(this);
+
+
+        this.interval = setInterval(this.load, 2000);
+    }
+
+});
--- public_html/webserver.js
+++ public_html/webserver.js
@@ -1,6 +1,16 @@
 (function () {
     var http = require('http');
     var express = require('express');
+    var fs = require('fs');
+    var app = express();
 
-    express.use(__dir + "/public/");
-}
+    const PORT = 3000;
+    http.createServer(app).listen(PORT);
+
+    app.use(express.static(__dirname + "/public"));
+
+    app.get('/log', function (req, res) {
+        var log = fs.readFileSync('../spigot/logs/latest.log', 'utf8');
+        res.end(log);
+    });
+})();
